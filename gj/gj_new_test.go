19de78586b8d4a9a87ffcc70167c8516a502d01c
Ivo van der Wijk <github@in.m3r.nl>
2021-02-26 17:17:27 +0100

    refactor tests

--- gj/gj_new_test.go
+++ gj/gj_new_test.go
@@ -7,6 +7,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type WrongTarget interface{}
+
+func FieldDecodeTests(t *testing.T, s NSerializer, target, value interface{}) {
+	sp := fmt.Sprintf
+	t.Run(sp("%#v Decode wrong target type", s), func(t *testing.T) {
+		assert := assert.New(t)
+		var target WrongTarget
+
+		err := s.Decode(&target, value)
+		assert.Error(err)
+		assert.Equal(ErrFieldIncorrectType, err)
+	})
+	t.Run(sp("%#v Decode No Pointer", s), func(t *testing.T) {
+		assert := assert.New(t)
+
+		err := s.Decode(target, value)
+		assert.Error(err)
+		assert.Equal(ErrFieldUnsettable, err)
+	})
+	t.Run(sp("%#v Decode nil", s), func(t *testing.T) {
+		assert := assert.New(t)
+
+		err := s.Decode(nil, value)
+		assert.Error(err)
+		assert.Equal(ErrFieldUnsettable, err)
+	})
+	t.Run(sp("%#v Decode wrong value", s), func(t *testing.T) {
+		assert := assert.New(t)
+
+		// Not sure if this will always work
+		err := s.Decode(&target, struct{}{})
+		assert.Error(err)
+		assert.Equal(ErrFieldDataIncorrectType, err)
+	})
+}
+
 func TestGJNew(t *testing.T) {
 	t.Run("Random", func(t *testing.T) {
 		assert := assert.New(t)
@@ -25,42 +61,13 @@ func TestGJNew(t *testing.T) {
 		fmt.Println(string(res))
 		// assert.True(false)
 	})
-	t.Run("Regular String Decode", func(t *testing.T) {
+
+	t.Run("StringField Regular Decode", func(t *testing.T) {
 		assert := assert.New(t)
 		var target string
 
 		assert.NoError(NStringField().Decode(&target, "Hello"))
-		assert.Equal("Hello", target)
+		assert.EqualValues("Hello", target)
 	})
-	t.Run("String Decode wrong target type", func(t *testing.T) {
-		assert := assert.New(t)
-		var target int64
-
-		err := NStringField().Decode(&target, "Hello")
-		assert.Error(err)
-		assert.Equal(ErrFieldIncorrectType, err)
-	})
-	t.Run("String Decode No Pointer", func(t *testing.T) {
-		assert := assert.New(t)
-		var target string
-
-		err := NStringField().Decode(target, "Hello")
-		assert.Error(err)
-		assert.Equal(ErrFieldUnsettable, err)
-	})
-	t.Run("String Decode nil", func(t *testing.T) {
-		assert := assert.New(t)
-
-		err := NStringField().Decode(nil, "Hello")
-		assert.Error(err)
-		assert.Equal(ErrFieldUnsettable, err)
-	})
-	t.Run("String Decode wrong value", func(t *testing.T) {
-		assert := assert.New(t)
-		var target string
-
-		err := NStringField().Decode(&target, 42)
-		assert.Error(err)
-		assert.Equal(ErrFieldDataIncorrectType, err)
-	})
+	FieldDecodeTests(t, NStringField(), "", "Hello")
 }
